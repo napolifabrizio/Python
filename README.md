974713d0723bb7b242be0505af4ea89a3a7880d7
Fabrizio <fabrizionapoli50@gmail.com>
2023-11-23 21:32:05 -0300

    detalhes

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Ao longo desse readme, vai ter mais detalhes sobre os projetos. Boa leitura!
 <h2>Demais projetos</h2>
 <p>
 jogoAdvinhacao.py --> Jogo aonde a máquina gera um número aleatório, entre 1 e 350, e o jogador tenta advinhar o número.
-<br>
+<br><br>
 duvidas.py --> Apenas um arquivo para tirar algumas dúvidas, ou fazer testes, sobre a linguagem/projeto.
 </p>
 </section>
--- duvidas.py
+++ duvidas.py
@@ -6,3 +6,21 @@ print(p1)
 number = "2"
 number = list(number)
 print(number)
+
+###################################################### List Comprehesion
+contas_idade = [14, 23, 16, 18, 32]
+maiores_de_idade = []
+
+for idade in contas_idade:
+  if idade >= 18:
+    maiores_de_idade.append(idade)
+
+print(maiores_de_idade)
+###########################################
+contas_idade2 = [14, 23, 16, 18, 32]
+
+maiores_de_idade2 = [idade for idade in contas_idade2 if idade >= 18]
+
+print(maiores_de_idade2)
+##################################################### List Comprehesion
+
