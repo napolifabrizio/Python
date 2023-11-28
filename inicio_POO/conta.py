b51ea67aa1e320056e75d763b6bd54fc82536de1
Fabrizio <fabrizionapoli50@gmail.com>
2023-11-28 00:34:04 -0300

    get_and_setters

--- inicio_POO/conta.py
+++ inicio_POO/conta.py
@@ -1,18 +1,42 @@
 
-
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Extrato: {self.saldo} | Titular: {self.titular}")
+        print(f"Extrato: {self.__saldo} | Titular: {self.__titular}")
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     
+    def __pode_sacar(self, valor):
+        if (valor > 0):
+            max_sacar = self.__saldo + self.__limite
+            return valor <= max_sacar
+
     def sacar(self, valor):
-        self.saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+    
+    def get_titular(self):
+        return self.__titular
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def get_numero(self):
+        return self.__numero
+    
+    def set_limite(self, limite):
+        self.__limite = limite
--- inicio_POO/main.py
+++ inicio_POO/main.py
@@ -1,4 +1,9 @@
 from conta import Conta
 
 conta_Fabrizio = Conta(123, "Fabrizio", 50000, 300000)
+conta_Peter = Conta(456, "Fabrizio", 30000, 100000)
+
+conta_Fabrizio.transferir(10000, conta_Peter)
+
 conta_Fabrizio.extrato()
+conta_Peter.extrato()
