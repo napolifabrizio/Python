845b5421e70c320838e801aa3d2d74235e02ffe9
Fabrizio <fabrizionapoli50@gmail.com>
2023-12-14 00:31:11 -0300

    duvidas

--- duvidas.py
+++ duvidas.py
@@ -16,7 +16,7 @@ for idade in contas_idade:
     maiores_de_idade.append(idade)
 
 print(maiores_de_idade)
-###########################################
+##
 contas_idade2 = [14, 23, 16, 18, 32]
 
 maiores_de_idade2 = [idade for idade in contas_idade2 if idade >= 18]
@@ -36,3 +36,73 @@ print("O diretório atual é:", diretorio_atual)
 idade = 10
 maior_de_idade = "maior de idade" if idade >= 18 else "menor de idade"
 print(maior_de_idade)
+
+
+nums = [1, 2, 3, 4, 5]
+
+# adiciona um elemento no final
+nums.append(6)
+
+# remove um elemento
+nums.remove(6)
+
+# remove todos os elementos
+nums.clear()
+
+# adiciona mais de um elemento
+nums.extend([1, 2, 3])
+print(nums)
+
+# recurso maneiro
+idades = [18, 19, 20, 21, 22]
+idades_ano_que_vem = [(idade+1) for idade in idades]
+print(idades_ano_que_vem)
+
+maior_21 = [idade for idade in idades if idade > 21]
+print(maior_21)
+
+# tuplas, tuplas são imutáveis
+fabrizio = ("fabrizio", 20)
+# fabrizio[1] = 21 --> isso vai dar erro
+# para mudar uma tupla, voce não a muda, vc muda a variavel. Olhe abaixo
+def acrescenta_idade(tupla, valor):
+    nova_idade = tupla[1] + valor
+    return (tupla[0], nova_idade)
+
+print(fabrizio)
+fabrizio = acrescenta_idade(fabrizio, 2)
+print(fabrizio)
+
+# imprimindo index e valor
+
+nums = [1, 2, 3, 4, 5]
+for i in range(len(nums)):
+   print(i, nums[i])
+
+for i, num in enumerate(nums):
+   print(i, num)
+
+# desempacotando
+
+usuarios = [
+   ("Fabrizio", 20, 2003),
+   ("Isabella", 24, 1999),
+   ("Talia", 1, 2022)
+]
+
+for nome, idade, ano_nascimento in usuarios:
+   print(nome, idade, ano_nascimento)
+
+for _, idade, _ in usuarios:
+   print(idade)
+
+# ordenando numeros
+
+nums = [10, 54, 1, 91, 29, 50, 33]
+sorted(nums) #crescente, não muda o array
+nums.sort() #crescente, muda o array
+
+# troca a ordem da lista, trazendo o inverso
+
+verso = [1, 2, 5, 3, 4]
+inverso = list(reversed(verso))
